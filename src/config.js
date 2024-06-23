3570bc3b872429db82a1cbf517a36d0f6240fe35
Lanthdev <lanthdev@gmail.com>
2024-06-22 23:32:56 -0500

    test

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-export const UNSPLASH_ACCESS_KEY = 'VLfvtl5BNYuGzVfnc-Kp7RJt4AIMjO2KLGTJYHAOOOo';
-export const PIXABAY_API_KEY = '44534170-c2a957bf7ded475d6dbf5c33f';
+export const UNSPLASH_ACCESS_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+export const PIXABAY_API_KEY = process.env.REACT_APP_PIXABAY_API_KEY;
